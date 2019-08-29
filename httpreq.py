ba27434e7f7468d36d7e03f4d5779d92a0bd854c
Chaitanya Tyagi <chay2199@gmail.com>
2019-08-30 02:21:53 +0530

    add

--- httpreq.py
+++ httpreq.py
@@ -2,6 +2,6 @@ import requests
 
 url = ('https://ci.appveyor.com/api/testresults/'
        'junit/$($env:APPVEYOR_JOB_ID)')
-files = {'file': open('$APPVEYOR_BUILD_FOLDER/report.xml', 'r')}
+files = {'file': open('C:/projects/coala-json/report.xml', 'r')}
 r = requests.post(url, files=files)
 print(r.text)
