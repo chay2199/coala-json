d87d1c8c0c2488074599635d1bec96809b73b097
Chaitanya Tyagi <chay2199@gmail.com>
2019-08-31 00:28:55 +0530

    add

--- coala-json_upload.py
+++ coala-json_upload.py
@@ -1,8 +1,9 @@
+import sys
 import requests
 
 
 with open('C:/projects/coala-json/report.xml', 'rb') as f:
     r = requests.post('https://ci.appveyor.com/api/testresults/'
-                      'junit/$($env:APPVEYOR_JOB_ID)',
+                      'junit/{}'.format(sys.argv[1:][0]),
                       files={'report.xml': f})
 print(r.url)
