29d624779e15872235f4d2583baa55ed30c6870f
Chaitanya Tyagi <chay2199@gmail.com>
2019-08-31 02:10:22 +0530

    add

--- coala_json/reporters/AppveyorReporter.py
+++ coala_json/reporters/AppveyorReporter.py
@@ -9,9 +9,8 @@ class AppveyorReporter(ResultReporter):
     """
     def to_output(self):
         appveyor_id = self.coala_json
-        print(appveyor_id.split('%')[0])
         with open('C:/projects/coala-json/report.xml', 'rb') as f:
             r = requests.post('https://ci.appveyor.com/api/testresults/'
-                              'junit/{}'.format(appveyor_id).split('%')[0],
+                              'junit/{}'.format(appveyor_id).split(' ')[0],
                               files={'report.xml': f})
         return r.url
