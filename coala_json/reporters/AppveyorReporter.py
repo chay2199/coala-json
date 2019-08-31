0a6d0e8d81ad3a65d3c3760bb1eaef08b8973346
Chaitanya Tyagi <chay2199@gmail.com>
2019-08-31 19:21:40 +0530

    add

--- coala_json/reporters/AppveyorReporter.py
+++ coala_json/reporters/AppveyorReporter.py
@@ -10,9 +10,8 @@ class AppveyorReporter(ResultReporter):
     """
 
     def to_output(self):
-        print(os.environ)
         file_to_upload = self.coala_json
-        appveyor_job_id = os.environ(['%APPVEYOR_JOB_ID%'])
+        appveyor_job_id = os.environ['APPVEYOR_JOB_ID']
         with open('C:/projects/coala-json/{}'.format(file_to_upload),
                   'rb') as f:
             r = requests.post('https://ci.appveyor.com/api/testresults/'
