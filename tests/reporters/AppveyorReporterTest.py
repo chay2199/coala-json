90d8919fc7fd001a8a188f2e3abc21bcb0c9dc82
Chaitanya Tyagi <chay2199@gmail.com>
2019-09-01 18:36:36 +0530

    add

--- tests/reporters/AppveyorReporterTest.py
+++ tests/reporters/AppveyorReporterTest.py
@@ -1,13 +1,28 @@
+import os
 import unittest
 
 from coala_json.reporters.AppveyorReporter import AppveyorReporter
 from coala_json.loader.coalaJsonLoader import coalaJsonLoader
 
 
+def get_path(filename):
+    file_path = os.path.join(os.path.dirname(__file__),
+                             'test_files/',
+                             filename)
+    return file_path
+
+
 class AppveyorReporterTest(unittest.TestCase):
 
-    def test_empty(self):
+    def test_file_not_found(self):
         loader = coalaJsonLoader()
         appveyor = AppveyorReporter(loader, 'report.xml')
         self.assertEqual(appveyor.to_output(),
                          'Permission denied or no such file or directory')
+
+    @unittest.skipUnless(os.getenv('APPVEYOR') == 'True', 'Skip unless AppVeyor'
+                                                          ' CI test')
+    def test_file(self):
+        loader = coalaJsonLoader()
+        appveyor = AppveyorReporter(loader, get_path('severity_info.xml'))
+        self.assertEqual(appveyor.to_output(), 400)
