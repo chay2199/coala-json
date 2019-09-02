2c815a81b7ee28fc5b9d007c66e349db9b8a0236
Chaitanya Tyagi <chay2199@gmail.com>
2019-09-02 21:21:40 +0530

    add

--- tests/reporters/AppveyorReporterTest.py
+++ tests/reporters/AppveyorReporterTest.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import requests_mock
+from unittest.mock import patch
 
 from coala_json.reporters.AppveyorReporter import AppveyorReporter
 from coala_json.loader.coalaJsonLoader import coalaJsonLoader
@@ -12,6 +14,7 @@ def get_path(filename):
     return file_path
 
 
+@requests_mock.mock()
 class AppveyorReporterTest(unittest.TestCase):
 
     def test_file_not_found(self):
@@ -20,9 +23,16 @@ class AppveyorReporterTest(unittest.TestCase):
         self.assertEqual(appveyor.to_output(),
                          'Permission denied or no such file or directory')
 
-    def test_file(self):
-        os.system('coala --json > empty.json')
-        os.system('coala-json --junit -f {} -o test.xml'.format('empty.json'))
-        loader = coalaJsonLoader()
-        appveyor = AppveyorReporter(loader, 'test.xml')
-        self.assertEqual(appveyor.to_output(), 204)
+    def test_file(self, m):
+        with patch.dict('os.environ', {'APPVEYOR_JOB_ID': '12345',
+                                       'APPVEYOR_BUILD_FOLDER': './'}):
+            loader = coalaJsonLoader()
+            appveyor = AppveyorReporter(loader, 'AppveyorReporterTest.py')
+            m.post('https://ci.appveyor.com/api/testresults/junit/{}'
+                   .format(os.getenv('APPVEYOR_JOB_ID')), status_code=200)
+            self.assertEqual(appveyor.to_output(),
+                             'https://ci.appveyor.com/api/testresults/junit/12345')
+
+
+if __name__ == '__main__':
+    unittest.main()
